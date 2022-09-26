acf804c64749db1869e7372ed1e8576dabbe53c7
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-09-26 17:18:31 -0300

    245

---
+++ assets/js/game.js
@@ -0,0 +1,122 @@
+// stat variables
+let cardsUncovers = 0;
+let card01 = null;
+let card02 = null;
+let firstResult = null;
+let secondResult = null;
+let movements = 0;
+let hits = 0;
+let timer = false;
+let startTimer = 99;
+let seconds = 99;
+let regresiveTime = null;
+
+let winAudio = new Audio('./assets/sound/win.wav');
+let loseAudio = new Audio('./assets/sound/lose.wav');
+let clickAudio = new Audio('./assets/sound/click.wav');
+let rightAudio = new Audio('./assets/sound/right.wav');
+let wrongAudio = new Audio('./assets/sound/wrong.wav');
+let timeAudio = new Audio('./assets/sound/time.wav');
+
+let playingAudio = new Audio('./assets/sound/playing.mp3');
+
+// a document html
+let showMovements = document.getElementById('movements');
+let showHits = document.getElementById('hits');
+let showTime = document.getElementById('time')
+
+
+// numbers
+let numbers = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14];
+numbers = numbers.sort( () => {return Math.random() - 0.5});
+console.log(numbers);
+
+// functions
+function countTime(){
+    playingAudio.play();
+    regresiveTime = setInterval(() => {
+        seconds--;
+        timeAudio.play();
+        showTime.innerHTML = `TIM ${seconds}`;
+        if(seconds == 0){
+            clearInterval(regresiveTime);
+            blockCards();
+            loseAudio.play();
+            playingAudio.pause();
+        }
+    }, 1500)
+}
+function blockCards(){
+    for (let i = 0; i <= 29; i++){
+        let cardBlock = document.getElementById(i);
+        cardBlock.innerHTML = `<img src="./assets/img/${numbers[i]}.png">`;
+        cardBlock.disabled = true;
+    }
+}
+
+// principal function
+function uncover(id){
+
+    if(timer == false){
+        countTime();
+        timer = true;
+    };
+
+    cardsUncovers++;
+    console.log(cardsUncovers);
+
+    if(cardsUncovers == 1){
+        // show first number
+        card01 = document.getElementById(id);
+        firstResult = numbers[id];
+        card01.innerHTML = `<img src="./assets/img/${firstResult}.png">`;
+
+        clickAudio.play();
+
+        // disable first button
+        card01.disabled = true;
+    }else if(cardsUncovers == 2){
+        // show second number
+        card02 = document.getElementById(id);
+        secondResult = numbers[id];
+        card02.innerHTML = `<img src="./assets/img/${secondResult}.png">`;
+
+        // disable second button
+        card02.disabled = true;
+
+        // add movements
+        movements++;
+        showMovements.innerHTML = `MOV ${movements}`;
+
+        if(firstResult == secondResult){
+            // to cero cards uncover
+            cardsUncovers = 0;
+
+            // add hits
+            hits++;
+            showHits.innerHTML = `HIT ${hits}`;
+
+            rightAudio.play();
+        }else{
+
+            wrongAudio.play();
+
+            // show timeout
+            setTimeout(() => {
+                card01.innerHTML = " ";
+                card02.innerHTML = " ";
+                card01.disabled = false;
+                card02.disabled = false;
+                cardsUncovers = 0;
+            }, 500)
+        }
+    };
+    if(hits == 16){
+        winAudio.play();
+        playingAudio.pause();
+        clearInterval(regresiveTime);
+        showHits.innerHTML = `HITS ${hits} 😱`;
+        showTime.innerHTML = `PERFECT, YOUR TIME IS ${startTimer - seconds} SECONDS!`;
+        showMovements.innerHTML = `MOV ${movements} 😎`;
+    }
+}
