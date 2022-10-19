9943668125defa7f24fc3c70ae0ec8a2cbf98d8e
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-19 11:16:30 -0300

    12

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,47 +28,44 @@ let showTime = document.getElementById('time')
 let showEndMsj = document.getElementById('end-msj')
 let resetButton = document.getElementById('restart-game')
 
+// add events
+document.getElementById('0').onclick = () => (uncover(0));
+document.getElementById('1').onclick = () => (uncover(1));
+document.getElementById('2').onclick = () => (uncover(2));
+document.getElementById('3').onclick = () => (uncover(3));
+document.getElementById('4').onclick = () => (uncover(4));
+document.getElementById('5').onclick = () => (uncover(5));
+document.getElementById('6').onclick = () => (uncover(6));
+document.getElementById('7').onclick = () => (uncover(7));
+document.getElementById('8').onclick = () => (uncover(8));
+document.getElementById('9').onclick = () => (uncover(9));
+document.getElementById('10').onclick = () => (uncover(10));
+document.getElementById('11').onclick = () => (uncover(11));
+document.getElementById('12').onclick = () => (uncover(12));
+document.getElementById('13').onclick = () => (uncover(13));
+document.getElementById('14').onclick = () => (uncover(14));
+document.getElementById('15').onclick = () => (uncover(15));
+document.getElementById('16').onclick = () => (uncover(16));
+document.getElementById('17').onclick = () => (uncover(17));
+document.getElementById('18').onclick = () => (uncover(18));
+document.getElementById('19').onclick = () => (uncover(19));
+document.getElementById('20').onclick = () => (uncover(20));
+document.getElementById('21').onclick = () => (uncover(21));
+document.getElementById('22').onclick = () => (uncover(22));
+document.getElementById('23').onclick = () => (uncover(23));
+document.getElementById('24').onclick = () => (uncover(24));
+document.getElementById('25').onclick = () => (uncover(25));
+document.getElementById('26').onclick = () => (uncover(26));
+document.getElementById('27').onclick = () => (uncover(27));
+document.getElementById('28').onclick = () => (uncover(28));
+document.getElementById('29').onclick = () => (uncover(29));
+
 // numbers
 let numbers = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14];
 numbers = numbers.sort( () => {return Math.random() - 0.5});
 console.log(numbers);
 
-// functions
-function countTime(){
-    playingAudio.play();
-    regresiveTime = setInterval(() => {
-        seconds--;
-        timeAudio.play();
-        showTime.innerHTML = `TIM ${seconds}`;
-        if(seconds == 0){
-            clearInterval(regresiveTime);
-            blockCards();
-            loseAudio.play();
-            playingAudio.pause();
-            showHits.innerHTML = ``;
-            showMovements.innerHTML = ``;
-            showTime.innerHTML = ``;
-            showEndMsj.innerHTML = `<span class="end-msj-looser">GOOD LOSER!</span>`;
-            resetButton.innerHTML = `<a class="reset-msj" onClick="buttonRestartAction()">Restart</a>`; 
-        }
-    }, 1100)
-}
-function blockCards(){
-    for (let i = 0; i <= 29; i++){
-        let cardBlock = document.getElementById(i);
-        cardBlock.innerHTML = `<img src="./assets/img/${numbers[i]}.png">`;
-        cardBlock.disabled = true;
-    }
-}
-function startSound(){
-    startSoundAudio.play();
-}
-function buttonRestartAction(){
-    startSoundAudio.play();
-    window.location.reload(true)
-}
-
-// principal function
+// main function
 function uncover(id){
 
     // starting timer
@@ -79,7 +76,7 @@ function uncover(id){
 
     // count cards
     cardsUncovers++;
-    // funni print in console
+    
     console.log(`HEEEE, WHAT DO YOU SEE? PLAY THE GAME 👀`);
 
     if(cardsUncovers == 1){
@@ -142,4 +139,39 @@ function uncover(id){
         resetButton.innerHTML = `<a class="reset-msj" onClick="window.location.reload(true)">Restart</a>`; 
 
     }
+}
+
+// functions
+function countTime(){
+    playingAudio.play();
+    regresiveTime = setInterval(() => {
+        seconds--;
+        timeAudio.play();
+        showTime.innerHTML = `TIM ${seconds}`;
+        if(seconds == 0){
+            clearInterval(regresiveTime);
+            blockCards();
+            loseAudio.play();
+            playingAudio.pause();
+            showHits.innerHTML = ``;
+            showMovements.innerHTML = ``;
+            showTime.innerHTML = ``;
+            showEndMsj.innerHTML = `<span class="end-msj-looser">GOOD LOSER!</span>`;
+            resetButton.innerHTML = `<a class="reset-msj" onClick="buttonRestartAction()">Restart</a>`; 
+        }
+    }, 1100)
+}
+function blockCards(){
+    for (let i = 0; i <= 29; i++){
+        let cardBlock = document.getElementById(i);
+        cardBlock.innerHTML = `<img src="./assets/img/${numbers[i]}.png">`;
+        cardBlock.disabled = true;
+    }
+}
+function startSound(){
+    startSoundAudio.play();
+}
+function buttonRestartAction(){
+    startSoundAudio.play();
+    window.location.reload(true)
 }
